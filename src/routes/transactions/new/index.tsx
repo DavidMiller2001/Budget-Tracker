cda594d2002714bf7cdebc4bcc53a4fcfcf1e619
David Miller <davidwaynemiller2001@gmail.com>
2026-05-06 19:54:07 -0400

    basic form for adding transactions

--- src/routes/transactions/new/index.tsx
+++ src/routes/transactions/new/index.tsx
@@ -1,9 +1,53 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Button } from '#/components/ui/button'
+import { Input } from '#/components/ui/input'
+import { createFileRoute, redirect } from '@tanstack/react-router'
+import { createServerFn, useServerFn } from '@tanstack/react-start'
+import { PlusIcon } from 'lucide-react'
+import { useRef } from 'react'
+import z from 'zod'
 
 export const Route = createFileRoute('/transactions/new/')({
   component: RouteComponent,
 })
 
+const addNewTransaction = createServerFn({ method: 'POST' })
+  .inputValidator(z.object({ amount: z.number() }))
+  .handler(async ({ data }) => {
+    const { db } = await import('#/db')
+    const { transactions } = await import('#/db/schema')
+    await db.insert(transactions).values({ amount: data.amount })
+    throw redirect({ to: '/' })
+  })
+
 function RouteComponent() {
-  return <div>Hello "/transactions/new/"!</div>
+  const amountRef = useRef<HTMLInputElement>(null)
+  const addNewTransactionFn = useServerFn(addNewTransaction)
+
+  function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
+    e.preventDefault()
+    const amount = amountRef.current?.value
+    if (!amount) return
+
+    const amountFormatted = parseInt(amount)
+    addNewTransactionFn({ data: { amount: amountFormatted } })
+  }
+
+  return (
+    <form onSubmit={(e) => handleSubmit(e)} className="flex gap-2">
+      <Input
+        autoFocus
+        ref={amountRef}
+        placeholder="Enter the amount"
+        className="flex-1"
+        aria-label="amount"
+        defaultValue={0}
+      />
+
+      <Button type="submit" asChild>
+        <span>
+          <PlusIcon /> Add
+        </span>
+      </Button>
+    </form>
+  )
 }

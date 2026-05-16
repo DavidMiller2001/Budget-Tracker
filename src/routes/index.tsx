cac225252bf5265ca4018b93b5c9b82b401a46b3
David Miller <davidwaynemiller2001@gmail.com>
2026-05-16 01:58:34 -0400

    add summary cards section

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,11 +11,13 @@ import {
   CardHeader,
   CardTitle,
 } from '#/components/ui/card'
+import { Field, FieldGroup, FieldLabel } from '#/components/ui/field'
+import { Progress } from '#/components/ui/progress'
 import type { categories } from '#/db/schema'
 
 import { createFileRoute, Link } from '@tanstack/react-router'
 import { createServerFn } from '@tanstack/react-start'
-import { Plus } from 'lucide-react'
+import { Plus, TrendingDown, TrendingUp } from 'lucide-react'
 const getTransactions = createServerFn({ method: 'GET' }).handler(async () => {
   const { db } = await import('#/db')
   const data = await db.query.transactions.findMany()
@@ -30,6 +32,25 @@ export const Route = createFileRoute('/')({
 function Home() {
   const { data } = Route.useLoaderData()
 
+  let totalIncome = 0
+  let totalExpense = 0
+
+  const expenseData: {
+    billAmount: number
+    foodAmount: number
+    entertainmentAmount: number
+    otherAmount: number
+  } = {
+    billAmount: 0,
+    foodAmount: 0,
+    entertainmentAmount: 0,
+    otherAmount: 0,
+  }
+
+  const expenseCategories = Object.keys(
+    expenseData,
+  ) as (keyof typeof expenseData)[]
+
   const transactionData: {
     category: (typeof categories)[number]
     amount: number
@@ -39,16 +60,32 @@ function Home() {
         category: 'Other',
         amount: 0,
       }
-    } else if (t.amount > 0) {
-      return {
-        category: t.category,
-        amount: t.amount,
-      }
+    }
+
+    if (t.category === 'Income') {
+      totalIncome += t.amount
     } else {
-      return {
-        category: t.category,
-        amount: Math.abs(t.amount),
-      }
+      totalExpense += t.amount
+    }
+
+    switch (t.category) {
+      case 'Bill':
+        expenseData.billAmount += t.amount
+        break
+      case 'Food':
+        expenseData.foodAmount += t.amount
+        break
+      case 'Entertainment':
+        expenseData.entertainmentAmount += t.amount
+        break
+      case 'Other':
+        expenseData.otherAmount += t.amount
+        break
+    }
+
+    return {
+      category: t.category,
+      amount: Math.abs(t.amount),
     }
   })
 
@@ -66,6 +103,55 @@ function Home() {
         </Button>
       </nav>
       <main className="flex flex-col p-4 gap-4 max-w-4xl mx-auto">
+        <div className="grid grid-cols-[1fr_2fr] grid-rows-2 gap-4">
+          <Card className="bg-primary-foreground flex justify-center">
+            <CardContent>
+              <div className="flex gap-2 items-center justify-around">
+                <div className="flex items-center justify-center bg-accent w-[50px] h-[50px] rounded-xl">
+                  <TrendingUp color="var(--color-primary)" />
+                </div>
+                <div>
+                  <p className="text-muted-foreground text-xs">
+                    Income this month
+                  </p>
+                  <h3 className="text-primary font-bold text-xl">{`$${totalIncome.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`}</h3>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+          <Card className="bg-primary-foreground col-1 flex justify-center">
+            <CardContent>
+              <div className="flex gap-2 items-center justify-around">
+                <div className="flex items-center justify-center bg-destructive/30 w-[50px] h-[50px] rounded-xl">
+                  <TrendingDown color="var(--color-destructive)" />
+                </div>
+                <div>
+                  <p className="text-muted-foreground text-xs">
+                    Expenses this month
+                  </p>
+                  <h3 className="text-destructive font-bold text-xl">{`$${Math.abs(totalExpense).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`}</h3>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+          <Card className="bg-primary-foreground row-start-1 row-span-2 col-2">
+            <CardHeader>
+              <CardTitle>Expenses by Category</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <FieldGroup>
+                {expenseCategories.map((category, i) => (
+                  <CategoryProgressBar
+                    key={i}
+                    category={category.replace('Amount', '') as string}
+                    amount={expenseData[category]}
+                    totalExpense={totalExpense}
+                  />
+                ))}
+              </FieldGroup>
+            </CardContent>
+          </Card>
+        </div>
         <Card className="bg-primary-foreground">
           <CardHeader>
             <CardTitle>
@@ -94,5 +180,31 @@ function Home() {
         </Card>
       </main>
     </>
+  )
+}
+
+function CategoryProgressBar(props: {
+  category: string
+  amount: number
+  totalExpense: number
+}) {
+  const { category, amount, totalExpense } = props
+  const percentage =
+    totalExpense !== 0 ? Math.abs((amount / totalExpense) * 100) : 0
+
+  return (
+    <Field>
+      <FieldLabel htmlFor={`${category.toLowerCase()}-bar`}>
+        <div className="flex justify-between items-end  w-full">
+          <span className="capitalize">{category}</span>
+          <span className="">{`$${Math.abs(amount)}`}</span>
+        </div>
+      </FieldLabel>
+      <Progress
+        id={`${category.toLowerCase()}-bar`}
+        value={percentage}
+        indicatorColor={`var(--${category.toLowerCase()}-color)`}
+      />
+    </Field>
   )
 }
